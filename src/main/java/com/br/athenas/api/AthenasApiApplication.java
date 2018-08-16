0b78ac7543d0387586a403494d83f1ff43c8e4e5
Your NameRafael <eudesrt@gmail.comm>
2018-08-16 20:24:34 -0300

    Implementação

---
+++ src/main/java/com/br/athenas/api/AthenasApiApplication.java
@@ -0,0 +1,12 @@
+package com.br.athenas.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AthenasApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AthenasApiApplication.class, args);
+	}
+}
